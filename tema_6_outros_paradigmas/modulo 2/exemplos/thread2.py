64bd96d1496120dfb4ac113db8c97109c778d233
Fransuelton <elton6103@gmail.com>
2024-04-02 13:36:21 -0300

    feat: adiciona thread1.py

---
+++ tema_6_outros_paradigmas/modulo 2/exemplos/thread2.py
@@ -0,0 +1,15 @@
+import threading
+import time
+
+
+# exemplo de funcao com parametros
+def funcao(mensagem):
+    for i in range(3):
+        print(i, mensagem)
+        time.sleep(1)
+
+
+print('Iniciando o programa!')
+x = threading.Thread(target=funcao, args=('Executando!',))
+x.start()
+print('Finalizando o programa!')
